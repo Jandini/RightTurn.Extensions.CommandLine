17035b3ae6dcfe7894d7340a3614c6eb9c52742e
Matt Janda <matt.janda.kingston@gmail.com>
2021-01-24 23:11:53 +0000

    feat: Add turn command line parser extensions

---
+++ src/RightTurn.Extensions.CommandLine/CommandLineTurn.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace RightTurn.Extensions.CommandLine
+{
+    public static class CommandLineTurn
+    {
+        public static int Take<TService, TImplementation, TOptions>(string[] args, Func<TService, int> run, Action<IServiceCollection> services = null)
+          where TOptions : class
+          where TService : class
+          where TImplementation : class, TService => new Turn()
+              .ParseOptions<TOptions>(args)
+              .WithOptionsAsSingleton<TOptions>()
+              .WithServices(services)
+              .Take<TService, TImplementation>(run);
+
+        public static void Take<TService, TImplementation, TOptions>(string[] args, Action<TService> run, Action<IServiceCollection> services = null)
+            where TOptions : class
+            where TService : class
+            where TImplementation : class, TService => new Turn()
+                .ParseOptions<TOptions>(args)
+                .WithOptionsAsSingleton<TOptions>()
+                .WithServices(services)
+                .WithDirections()
+                .Take<TService, TImplementation>(run);
+
+        public static void Take<TService, TImplementation>(string[] args, Action<TService> run, Action<IServiceCollection> services = null)
+            where TService : class
+            where TImplementation : class, TService => new Turn()
+                .ParseVerbs(args)
+                .WithServices(services)
+                .Take<TService, TImplementation>((service) => { run.Invoke(service); return 0; });
+
+
+        public static int Take<TService, TImplementation>(string[] args, Func<TService, int> run, Action<IServiceCollection> services = null)
+            where TService : class
+            where TImplementation : class, TService => new Turn()
+                .ParseVerbs(args)
+                .WithServices((services) => services.AddTransient<TService, TImplementation>())
+                .WithServices(services)
+                .Take<TService, TImplementation>(run);
+
+
+        public static TService Take<TService, TImplementation>(string[] args, Action<IServiceCollection> services = null)
+            where TService : class
+            where TImplementation : class, TService => new Turn()
+                .ParseVerbs(args)
+                .WithServices((services) => { services.AddTransient<TService, TImplementation>(); })
+                .WithServices(services)
+                .Take()
+                .GetRequiredService<TService>();
+
+        public static int Take(string[] args, Action<IServiceCollection> services, Func<IServiceProvider, int> run) => new Turn()
+            .ParseVerbs(args)
+            .WithServices(services)
+            .Take(run);
+    }
+}
---
+++ src/RightTurn.Extensions.CommandLine/TurnDirectionsExtensions.cs
@@ -0,0 +1,11 @@
+﻿using CommandLine;
+using System;
+
+namespace RightTurn.Extensions.CommandLine
+{
+    public static class TurnDirectionsExtensions
+    {
+        public static Type[] GetVerbs(this ITurnDirections directions) => directions.TryGet<Type[]>();
+        public static Parser GetParser(this ITurn turn) => turn.Directions.TryGet<Parser>() ?? Parser.Default;
+    }
+}
